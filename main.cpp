b7d57f55d28133114791dcfbc9f146b98d5227a5
James Arthur <jamesarthur7@icloud.com>
2019-01-27 16:03:20 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include "windows.h"
+#include "Countingmech.h"
+
+using namespace std;
+
+int e;
+
+int main() {
+	Countingmech();
+	cin >> e;
+	return 0;
+}
